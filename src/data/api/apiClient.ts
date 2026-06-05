591ff944870bd9987a82d8de8b38b3d30189a864
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2026-06-05 10:08:06 +0700

    perbaiki interceptor request untuk menambahkan token pengguna pada header

--- src/data/api/apiClient.ts
+++ src/data/api/apiClient.ts
@@ -12,6 +12,10 @@ const apiClient = axios.create({
 apiClient.interceptors.request.use(
     async (config) => {
         const token = await SecureStore.getItemAsync('user_token');
-
-    }
-)
+        if (token && config.headers) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
